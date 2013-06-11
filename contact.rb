29de650aa0fc4ea28e318961680bf0c82ed83c4d
coolmatt <themattemmons@gmail.com>
2013-06-11 13:04:42 -0400

    Starting Contact class

--- contact.rb
+++ contact.rb
@@ -2,7 +2,12 @@ class Contact
 
   attr_accessor
 
-  def initialize
+  def initialize(id, firstname, lastname, email, notes)
+    @id = id
+    @firstname = firstname
+    @lastname = lastname
+    @email = email
+    @notes = notes
   end
 
   def display_contact
--- test/test_contact.rb
+++ test/test_contact.rb
@@ -5,6 +5,15 @@ require_relative "../contact"
 class TestContact < MiniTest::Unit::TestCase
 
   def test_setup
+    @contact = Contact.new(1000, "Frank", "Budkis", "email@email.com", "notes here")
+  end
+
+  def test_display_contact
+    assert_equal "First name: #{@contact.firstname}\n
+                  Last name: #{@contact.lastname}\n
+                  Email: #{@contact.email}\n
+                  Notes: #{@contact.notes}",
+                  @contact.display_contact
   end
 
 
